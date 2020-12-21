99f6eaf9d500ac61f38d6774ba46a3e5ba167f2c
shreyagupta09 <75797241+shreyagupta09@users.noreply.github.com>
2020-12-21 22:14:12 +0530

    Add files via upload
    Voice communicator is a superb game. If you click on any of the button it will say that sentence that was written on the button .

---
+++ code.js
@@ -0,0 +1,25 @@
+onEvent("button14", "click", function( ) {
+  setScreen("mainAppScreen");
+});
+onEvent("hello1button", "click", function( ) {
+  playSound("assets/helloshreya.mp3");
+});
+onEvent("button2whatname", "click", function( ) {
+  playSound("assets/shreyaname.mp3");
+});
+onEvent("button3howyou", "click", function( ) {
+  playSound("assets/shreyahowru.mp3");
+});
+onEvent("button4howold", "click", function( ) {
+  playSound("assets/shreyahowold.mp3");
+});
+onEvent("button5wherelive", "click", function( ) {
+  playSound("assets/shreyawherelive.mp3");
+});
+onEvent("button6whichschool", "click", function( ) {
+  playSound("assets/shreyawhichschol.mp3");
+});
+onEvent("button7whattime", "click", function( ) {
+  playSound("assets/shreyatime.mp3");
+});
+
